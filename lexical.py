af3302e632aa0549809f15c8f0cc10cb36d5318b
Shreya Paithani <152017971+shreyapaithani@users.noreply.github.com>
2025-05-13 20:47:30 +0530

    lexical.py

---
+++ lexical.py
@@ -0,0 +1,29 @@
+import re
+
+# ------------------ PHASE 1: Lexical Analysis ------------------ #
+def tokenize(code):
+    token_specification = [
+        ('NUMBER',   r'\d+'),
+        ('ADD',      r'\+'),
+        ('SUB',      r'-'),
+        ('MUL',      r'\*'),
+        ('DIV',      r'/'),
+        ('LPAREN',   r'\('),
+        ('RPAREN',   r'\)'),
+        ('SKIP',     r'[ \t]+'),
+        ('MISMATCH', r'.')
+    ]
+    tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
+    tokens = []
+    for mo in re.finditer(tok_regex, code):
+        kind = mo.lastgroup
+        value = mo.group()
+        if kind == 'NUMBER':
+            tokens.append(('NUMBER', int(value)))
+        elif kind in ('ADD', 'SUB', 'MUL', 'DIV', 'LPAREN', 'RPAREN'):
+            tokens.append((kind, value))
+        elif kind == 'SKIP':
+            continue
+        else:
+            raise RuntimeError(f'Unexpected character: {value}')
+    return tokens
